b76050c0c1abe070548e799acdd9fd90efdd95fc
Navian <primeguyy@gmail.com>
2021-08-26 15:19:24 +0000

    simple word counter

--- alpha/views.py
+++ alpha/views.py
@@ -4,12 +4,19 @@ from django.http import HttpResponse
 
 def index(request):
     # name = "Humphrey"
-    context = {
-        'name': 'Meghan Markle',
-        'age': 42,
-        'height': 145.8,
-        'nationality': 'Canada'
-    }
+    # context = {
+    #     'name': 'Meghan Markle',
+    #     'age': 42,
+    #     'height': 145.8,
+    #     'nationality': 'Canada'
+    # }
 
-    return render(request, "index.html", context)
+    return render(request, 'wordcounter.html')
 
+
+# word counter function
+def counter(request):
+    words = request.GET['words']
+    number_of_words = len(words.split())
+
+    return render(request, 'countedwords.html', {'number_of_words': number_of_words})
