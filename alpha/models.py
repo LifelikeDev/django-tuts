2d48bfe03e3fc0d1083c26a2efd6dd6a5fa27f75
Navian <primeguyy@gmail.com>
2021-08-30 19:28:25 +0000

    using models to dynamically render data in static html files

--- alpha/models.py
+++ alpha/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Features:
+    id: int
+    title: str
+    description: str
--- alpha/views.py
+++ alpha/views.py
@@ -1,17 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Features
 
 
 def index(request):
-    # name = "Humphrey"
-    # context = {
-    #     'name': 'Meghan Markle',
-    #     'age': 42,
-    #     'height': 145.8,
-    #     'nationality': 'Canada'
-    # }
+    feature1 = Features()
+    feature2 = Features()
+    feature3 = Features()
+    feature4 = Features()
 
-    return render(request, 'wordcounter.html')
+    feature1.id = 1
+    feature1.title = "Easy to use"
+    feature1.description = "As simple as ABC and \"I-can-see\""
+
+    feature2.id = 2
+    feature2.title = "Reliable"
+    feature2.description = "Built to be trusted by many"
+
+    feature3.id = 3
+    feature3.title = "Secure"
+    feature3.description = "Bonds that can never break"
+
+    feature4.id = 4
+    feature4.title = "Resilient"
+    feature4.description = "Passed through the cold and the heat"
+
+    features = [feature1, feature2, feature3, feature4]
+
+    return render(request, 'wordcounter.html', {"features": features})
 
 
 # word counter function
